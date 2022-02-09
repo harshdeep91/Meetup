76b96437aa419644695e647ee2c441f8815c5616
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-02-09 21:02:54 +0530

    api routes

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -2,8 +2,45 @@ import Meetupdetails from "../../components/meetups/Meetupdetails";
 
 function DetailPage(props) {
     return <>
-    <Meetupdetails img="https://www.barnesandnoble.com/blog/wp-content/uploads/2019/11/lightspeed113.png" title="hi" description="hello"/>
+    <Meetupdetails img={props.meetupdata.img} title={props.meetupdata.title} description={props.meetupdata.description} address={props.meetupdata.address} id={props.meetupdata.id}/>
     </>
 }
-
+export async function getStaticPaths(){
+    return {
+          fallback:false, 
+             paths:[
+                 {
+                     params:{
+                     meetupid:'1',
+                      },
+                 },
+                {
+                    params:{
+                    meetupid:'2'
+                     }
+               },
+               {
+                params:{
+                meetupid:'3'
+                 }
+               }
+                ]
+    }
+}
+export async function getStaticProps(context){
+    //fetch data from api
+const meetupId=context.params.meetupid;
+          return {
+            props:{
+                  meetupdata:{
+                      img:'https://www.barnesandnoble.com/blog/wp-content/uploads/2019/11/lightspeed113.png',
+                      title:'hi',
+                      description:'hello',
+                      id:meetupId,
+                      address:'halua',
+                      description:'rehne'
+                  }
+            }
+      };
+    }
 export default DetailPage;
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,21 @@
 import MeetupList from "../components/meetups/MeetupList";
 const meetups=[
     {
-          id:1,
+          id:'1',
           image:"https://www.barnesandnoble.com/blog/wp-content/uploads/2019/11/lightspeed113.png",
           title:"First",
           address:"panjra"
     },
     {
      
-          id:2,
+          id:'2',
           image:"https://www.barnesandnoble.com/blog/wp-content/uploads/2019/11/lightspeed113.png",
           title:"Second",
           address:"panjra"
     },
     {
      
-          id:3,
+          id:'3',
           image:"https://www.barnesandnoble.com/blog/wp-content/uploads/2019/11/lightspeed113.png",
           title:"Third",
           address:"panjra"
@@ -25,11 +25,25 @@ const meetups=[
 function HomePage(props) {
     return <MeetupList meetups={props.meeting}/>
 }
+// export async function getServerSideProps(context){
+//       const req=context.req;
+//       const res=context.res;
+//       //this will update every server request
+//       // but problem is when we dont want to reload every request(when our content not updated frequently)
+//       return {
+//             props:{
+//                   meeting:meetups
+//             },
+  
+//       };
+// }
 export async function getStaticProps(){
-  return {
+//fetch data from api 
+      return {
         props:{
               meeting:meetups
-        }
+        },
+        revalidate:10
   };
 }
 export default HomePage;
