847939020cbf6a41d8afb405ba4db07585bac039
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-02-12 16:45:37 +0530

    pre rendering

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,30 +1,19 @@
 import Meetupdetails from "../../components/meetups/Meetupdetails";
-
+import { MongoClient } from "mongodb";
 function DetailPage(props) {
     return <>
     <Meetupdetails img={props.meetupdata.img} title={props.meetupdata.title} description={props.meetupdata.description} address={props.meetupdata.address} id={props.meetupdata.id}/>
     </>
 }
 export async function getStaticPaths(){
+    const client=await MongoClient.connect('mongodb+srv://harshdeep:wnEWyn2p9WqRwMz@cluster0.x4gkf.mongodb.net/meetups?retryWrites=true&w=majority');
+const db=client.db();
+const meetupcollections=db.collection('meetups');
+const data=await meetupcollections.find({},{_id:1}).toArray();
+client.close();
     return {
           fallback:false, 
-             paths:[
-                 {
-                     params:{
-                     meetupid:'1',
-                      },
-                 },
-                {
-                    params:{
-                    meetupid:'2'
-                     }
-               },
-               {
-                params:{
-                meetupid:'3'
-                 }
-               }
-                ]
+         paths:data.map(d=>({params:{meetupid:d._id.toString()}}))
     }
 }
 export async function getStaticProps(context){
