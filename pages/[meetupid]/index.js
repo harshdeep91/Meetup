fe20ab8843fc130dffe5254ce23d167ad57804ff
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-02-12 16:57:05 +0530

    Learned how to generate page dynamically and fetch

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,8 +1,8 @@
 import Meetupdetails from "../../components/meetups/Meetupdetails";
-import { MongoClient } from "mongodb";
+import { MongoClient,ObjectId} from "mongodb";
 function DetailPage(props) {
     return <>
-    <Meetupdetails img={props.meetupdata.img} title={props.meetupdata.title} description={props.meetupdata.description} address={props.meetupdata.address} id={props.meetupdata.id}/>
+    <Meetupdetails img={props.meetupdata.img} title={props.meetupdata.title} description={props.meetupdata.address} address={props.meetupdata.address} />
     </>
 }
 export async function getStaticPaths(){
@@ -19,15 +19,18 @@ client.close();
 export async function getStaticProps(context){
     //fetch data from api
 const meetupId=context.params.meetupid;
+const client=await MongoClient.connect('mongodb+srv://harshdeep:wnEWyn2p9WqRwMz@cluster0.x4gkf.mongodb.net/meetups?retryWrites=true&w=majority');
+const db=client.db();
+const meetupcollections=db.collection('meetups');
+const data=await meetupcollections.findOne({_id:ObjectId(meetupId)});
+client.close();
           return {
             props:{
                   meetupdata:{
-                      img:'https://www.barnesandnoble.com/blog/wp-content/uploads/2019/11/lightspeed113.png',
-                      title:'hi',
-                      description:'hello',
-                      id:meetupId,
-                      address:'halua',
-                      description:'rehne'
+                      id:data._id.toString(),
+                      title:data.title,
+                      address:data.address,
+                      img:data.image
                   }
             }
       };
